5c9802222ebe656b1208fb57c0c059eb36ad3439
Tingel He <tingeljin@gmail.com>
2013-01-20 23:32:45 +0800

    themes update
    更新了主题的架构

--- protected/controllers/WmController.php
+++ protected/controllers/WmController.php
@@ -6,34 +6,16 @@ class WmController extends Controller
 	
 	public function actionIndex()
 	{
-		//$this->layout='../themes/shpd2012/views/layouts/column1.php';
 		$this->render('index');
 	}
-
-	// Uncomment the following methods and override them if needed
-	/*
-	public function filters()
+	public function actionShopList()
 	{
-		// return the filter configuration for this controller, e.g.:
-		return array(
-			'inlineFilterName',
-			array(
-				'class'=>'path.to.FilterClass',
-				'propertyName'=>'propertyValue',
-			),
-		);
+		$this->render('shoplist');
+	}
+	public function actionShop()
+	{
+		$this->render('shop');
 	}
 
-	public function actions()
-	{
-		// return external action classes, e.g.:
-		return array(
-			'action1'=>'path.to.ActionClass',
-			'action2'=>array(
-				'class'=>'path.to.AnotherActionClass',
-				'propertyName'=>'propertyValue',
-			),
-		);
-	}
-	*/
+
 }
--- themes/shpd2012/views/layouts/wmcolumn.php
+++ themes/shpd2012/views/layouts/wmcolumn.php
@@ -1,6 +1,10 @@
 <?php /* @var $this Controller */ ?>
 <?php $this->beginContent('//layouts/wmmain'); ?>
-<div id="content">
-	<?php echo $content; ?>
-</div><!-- content -->
+<div class="bodyer_css row-fluid ">
+	<div class="span10 offset1">
+		<?php echo $content; ?>
+	</div>
+	
+</div><!-- mainContent -->
+
 <?php $this->endContent(); ?>
