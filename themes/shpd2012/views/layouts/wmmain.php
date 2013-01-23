81b5e02736908da854e1aaef7bc9f6092c344292
Tingel He <tingeljin@gmail.com>
2013-01-23 18:25:01 +0800

    themes update
    主题优化

--- themes/shpd2012/views/layouts/wmmain.php
+++ themes/shpd2012/views/layouts/wmmain.php
@@ -10,14 +10,34 @@
 <!--  <link href="<?php echo Yii::app()->theme->baseUrl; ?>/css/bootstrap-responsive.css" rel="stylesheet" media="screen">-->
 <link href="<?php echo Yii::app()->theme->baseUrl; ?>/css/shpdmain.css" rel="stylesheet" media="screen">
 <!-- <script src="http://code.jquery.com/jquery-latest.js"></script> -->
-<!-- <script src="<?php echo Yii::app()->theme->baseUrl; ?>/js/bootstrap.min.js"></script> -->
+
 </head>
 <body>
 <!-- header start -->
 	<div class="hearder_css">
-		<div class="row-fluid header_toper"></div>
+		<div class="header_toper"></div>
 		<div class="container header_logor">
-			<div class="span11"><h1>Logo</h1></div>
+			<div class="row">
+				<div class="span3"><?php echo CHtml::image(Yii::app()->theme->baseUrl.'/img/logo2.png')?></div>
+				<div class="span6">
+					<div class="tabbable"> <!-- Only required for left/right tabs -->
+					  <ul class="nav nav-tabs">
+					    <li class="active"><a href="#tab1" data-toggle="tab">Section 1</a></li>
+					    <li><a href="#tab2" data-toggle="tab">Section 2</a></li>
+					  </ul>
+					  <div class="tab-content">
+					    <div class="tab-pane active input-append" id="tab1">
+					  <input class="span4" id="appendedInputButton" type="text" placeholder="找店铺">
+					  <button class="btn btn-info" type="button">Search</button>
+					    </div>
+					    <div class="tab-pane input-append" id="tab2">
+					  <input class="span4" id="appendedInputButton" type="text" placeholder="找美食">
+					  <button class="btn btn-info" type="button">Search</button>
+					    </div>
+					  </div>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 <!-- header end -->
@@ -29,6 +49,7 @@
 </div>
 <!-- footer end -->
 </body>
+<script src="<?php echo Yii::app()->theme->baseUrl; ?>/js/bootstrap.min.js"></script>
 </html>
 
 
--- themes/shpd2012/views/wm/shoplist.php
+++ themes/shpd2012/views/wm/shoplist.php
@@ -1,13 +1,14 @@
-<div class="row-fluid">
-	<div class="span9 ">
+<div class="row">
+	<div class="span9_5 ">
 		<?php $this->widget('zii.widgets.CListView', array(
 			'dataProvider'=>$dataProvider,
 			'itemView'=>'_shoplist',
-			'template'=>'{summary}<div class="row-fluid">{items}</div><div class="pager">{pager}</div>',
+			'template'=>'{summary}<div class="row marginleft10">{items}</div><div class="pager">{pager}</div>',
 			'itemsCssClass'=>'thumbnails',
-			'summaryCssClass'=>'row-fluid',//定义summary的div容器的class
+			'itemsTagName'=>'ul',
+			'summaryCssClass'=>'row ybxj dise shangxiabianjie',//定义summary的div容器的class
 			'summaryText'=>'<h5>在佛山市/南海区/某商业圈共找到{count}家外卖餐馆(面包屑)当前页显示第{start}-{end}家</h5>',
 		)); ?>
 	</div>
-	<div class="span3"></div>
+	<div class="span2_5"></div>
 </div>
